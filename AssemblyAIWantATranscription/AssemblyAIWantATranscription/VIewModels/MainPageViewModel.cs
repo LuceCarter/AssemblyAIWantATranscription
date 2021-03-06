160823aca1d2c18821ce002845f28403e64dd721
Luce Carter <luce@lucyscarter.co.uk>
2021-03-06 11:49:00 +0000

    :building_construction: Implemented MVVM Functionality

--- AssemblyAIWantATranscription/AssemblyAIWantATranscription/VIewModels/MainPageViewModel.cs
+++ AssemblyAIWantATranscription/AssemblyAIWantATranscription/VIewModels/MainPageViewModel.cs
@@ -1,10 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
+using MvvmHelpers;
+using Xamarin.Forms;
 
-namespace AssemblyAIWantATranscription.VIewModels
+namespace AssemblyAIWantATranscription.ViewModels
 {
-    class MainPageViewModel
+    public class MainPageViewModel : BaseViewModel
     {
+        
+
+
+        public MainPageViewModel()
+        {
+        }
+
+        private string _transcribedTextLabel = "";
+        public string TranscribedTextLabel
+        {
+            get => _transcribedTextLabel;
+            set => SetProperty(ref _transcribedTextLabel, value);
+        }
+
+        public ICommand RecordButtonCommand
+        {
+            get
+            {
+                return new Command(() =>
+                {
+                    TranscribedTextLabel = "Button was pressed!";
+                });
+            }
+        }
+
+        private void RecordAudio()
+        {
+            Console.WriteLine("I recorded Audio!");
+        }
     }
 }
